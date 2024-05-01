0998847e5b6a2ad736624790825f7bbd6b6ae122
Reahana <reahana233@gmail.com>
2024-05-01 19:22:27 +0600

    project

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmSnp2uTJpo5zoDbL9xAgRwwSwHKtNxtE",
+  authDomain: "auth-firebase-context-ta-ff148.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-ff148",
+  storageBucket: "auth-firebase-context-ta-ff148.appspot.com",
+  messagingSenderId: "205867929765",
+  appId: "1:205867929765:web:41c2576dc23ff375887d4c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar bg-neutral text-neutral-content">
+                <button className="btn btn-ghost text-xl">Auth Master</button>
+                <Link className="btn btn-ghost text-xl" to='/'>Home </Link>
+                <Link className="btn btn-ghost text-xl" to='/login'>Login </Link>
+                <Link className="btn btn-ghost text-xl" to='/register'>Register </Link>
+            </div>
+            
+          
+        </div>
+    );
+};
+
+export default Header;
