d2dc4df6b42e5ff019087ffe30c3393625541320
Reahana <reahana233@gmail.com>
2024-05-02 13:10:02 +0600

    all

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProviders';
 
 const Header = () => {
+    const { user,logOut } = useContext(AuthContext);
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.error(error))
+    }
+
     return (
         <div>
             <div className="navbar bg-neutral text-neutral-content">
                 <button className="btn btn-ghost text-xl">Auth Master</button>
                 <Link className="btn btn-ghost text-xl" to='/'>Home </Link>
+                <Link className="btn btn-ghost text-xl" to='/orders'>Orders </Link>
+               {user && <Link className="btn btn-ghost text-xl" to='/profile'>Profile </Link>} 
                 <Link className="btn btn-ghost text-xl" to='/login'>Login </Link>
                 <Link className="btn btn-ghost text-xl" to='/register'>Register </Link>
+                {
+                    user ? <>
+                        <span>{user.email}</span>
+                        <button onClick={handleLogOut} className="btn btn-xs"> Sign out</button>
+                    </> : <Link to="/login">Login </Link>
+                }
+            
+            
             </div>
             
           
--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import {  createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import {  createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../Firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -9,6 +9,7 @@ const auth = getAuth(app);
 const AuthProviders = ({children}) => {
 
 const [user, setUser] = useState(null);
+const [loading, setLoading] = useState(true);
 
 const createUser = (email,password)=>{
     return createUserWithEmailAndPassword(auth, email, password)
@@ -16,9 +17,21 @@ const createUser = (email,password)=>{
 const signIn  = (email,password)=>{
     return signInWithEmailAndPassword(auth, email, password)
 }
-
+const logOut = ()=>{
+    return signOut(auth);
+}
+ useEffect( ()=>{
+   const unsubscribe =  onAuthStateChanged(auth,currentUser =>{
+        console.log(currentUser);
+        setUser(currentUser);
+        setLoading(false);
+    })
+    return () =>{
+        unsubscribe();
+    }
+ })
 const authInfo ={
-    user, createUser, signIn
+    user, createUser, signIn, logOut,  loading
 }
 
     //const user = {displayName : 'Asrafi'}
